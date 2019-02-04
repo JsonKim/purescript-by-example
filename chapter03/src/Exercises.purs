0fcd783ce2fe526cd35f903646122e54f01cbff6
Jason Kim <actcrazy@gmail.com>
2019-02-04 14:24:57 +0900

    update chapter 3

--- chapter03/src/Exercises.purs
+++ chapter03/src/Exercises.purs
@@ -25,3 +25,33 @@ squares = map (\n -> n * n)
 
 removeNagatives :: Array Int -> Array Int
 removeNagatives = filter (\n -> n > 0)
+
+factors :: Int -> Array (Array Int)
+factors n = filter (\xs -> product xs == n) $ do
+  i <- 1 .. n
+  j <- i .. n
+  pure [i, j]
+
+factors' :: Int -> Array (Array Int)
+factors' n = do
+  i <- 1 .. n
+  j <- i .. n
+  guard $ i * j == n
+  pure [i, j]
+
+isPrime :: Int -> Boolean
+isPrime n = (length $ factors' n) == 1
+
+cartesianProduct :: forall a. Array a -> Array a -> Array (Array a)
+cartesianProduct x y = do
+  xs <- x
+  ys <- y
+  pure [xs, ys]
+
+triples :: Int -> Array (Array Int)
+triples n = do
+  a <- 1 .. n
+  b <- a .. n
+  c <- b .. n
+  guard $ c * c == a * a + b * b
+  pure [a, b, c]
