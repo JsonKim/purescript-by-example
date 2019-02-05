14b2db78815d7944fe6e80a4ff03e9c40e4c862a
Jason Kim <actcrazy@gmail.com>
2019-02-05 15:48:46 +0900

    add Data.Picture

---
+++ chapter05/src/Data/Picture.purs
@@ -0,0 +1,37 @@
+module Data.Picture where
+
+import Prelude
+
+data Point = Point
+  { x :: Number
+  , y :: Number
+  }
+
+showPoint :: Point -> String
+showPoint (Point { x, y }) =
+  "(" <> show x <> ", " <> show y <> ")"
+
+data Shape
+  = Circle Point Number
+  | Rectangle Point Number Number
+  | Line Point Point
+  | Text Point String
+
+showShape :: Shape -> String
+showShape (Circle c r) =
+  "Circle [center: " <> showPoint c <> ", radius: " <> show r <> "]"
+showShape (Rectangle c w h) =
+  "Rectangle [center: " <> showPoint c <> ", width: " <> show w <> ", height: " <> show h <> "]"
+showShape (Line start end) =
+  "Line [start: " <> showPoint start <> ", end: " <> showPoint end <> "]"
+showShape (Text loc text) =
+  "Text [location: " <> showPoint loc <> ", text: " <> show text <> "]"
+
+exampleLine :: Shape
+exampleLine = Line p1 p2
+  where
+    p1 :: Point
+    p1 = Point { x: 0.0, y: 0.0 }
+
+    p2 :: Point
+    p2 = Point { x: 100.0, y: 50.0 }
