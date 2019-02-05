f29be5c7661a46e095eb003dcbd8ec82bbf06397
Jason Kim <actcrazy@gmail.com>
2019-02-05 14:17:16 +0900

    add 4.17
    @TODO
    나머지 문제들도 풀어볼 것

--- chapter04/src/FileOperations.purs
+++ chapter04/src/FileOperations.purs
@@ -2,8 +2,8 @@ module FileOperstions where
 
 import Prelude
 
-import Data.Path (Path, ls)
-import Data.Array (concatMap, (:))
+import Data.Path (Path, ls, isDirectory)
+import Data.Array (concatMap, (:), filter)
 
 allFiles :: Path -> Array Path
 allFiles file = file : concatMap allFiles (ls file)
@@ -12,3 +12,15 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+onlyFiles :: Path -> Array Path
+onlyFiles file = filter (not isDirectory) $ allFiles file
+
+{--
+whereIs :: String -> Maybe Path
+whereIs file = do
+  path <- allFiles' root
+  child <- ls path
+  guard $ (filename child) == file
+  Just path
+--}
