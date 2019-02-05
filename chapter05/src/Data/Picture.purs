a0e774cf0f6b3e34d445a30e5e04f4393f7b6681
Jason Kim <actcrazy@gmail.com>
2019-02-05 17:12:28 +0900

    chapter 5 코드 추가

--- chapter05/src/Data/Picture.purs
+++ chapter05/src/Data/Picture.purs
@@ -3,6 +3,9 @@ module Data.Picture where
 import Prelude
 
 import Data.Maybe (Maybe(..))
+import Data.Foldable (foldl)
+import Global as Global
+import Math as Math
 
 data Point = Point
   { x :: Number
@@ -56,3 +59,92 @@ scale = map scale' where
 getText :: Shape -> Maybe String
 getText (Text _ text) = Just text
 getText _ = Nothing
+
+type Picture = Array Shape
+
+showPicture :: Picture -> Array String
+showPicture = map showShape
+
+data Bounds = Bounds
+  { top    :: Number
+  , left   :: Number
+  , bottom :: Number
+  , right  :: Number
+  }
+
+showBounds :: Bounds -> String
+showBounds (Bounds b) =
+  "Bounds [top: " <> show b.top <>
+  ", left: "      <> show b.left <>
+  ", bottom: "    <> show b.bottom <>
+  ", right: "     <> show b.right <>
+  "]"
+
+shapeBounds :: Shape -> Bounds
+shapeBounds (Circle (Point { x, y }) r) = Bounds
+  { top:    y - r
+  , left:   x - r
+  , bottom: y + r
+  , right:  x + r
+  }
+shapeBounds (Rectangle (Point { x, y }) w h) = Bounds
+  { top:    y - h / 2.0
+  , left:   x - w / 2.0
+  , bottom: y + h / 2.0
+  , right:  x + w / 2.0
+  }
+shapeBounds (Line (Point p1) (Point p2)) = Bounds
+  { top:    Math.min p1.y p2.y
+  , left:   Math.min p1.x p2.x
+  , bottom: Math.max p1.y p2.y
+  , right:  Math.max p1.x p2.x
+  }
+shapeBounds (Text (Point { x, y }) _) = Bounds
+  { top:    y
+  , left:   x
+  , bottom: y
+  , right:  x
+  }
+
+union :: Bounds -> Bounds -> Bounds
+union (Bounds b1) (Bounds b2) = Bounds
+  { top:    Math.min b1.top    b2.top
+  , left:   Math.min b1.left   b2.left
+  , bottom: Math.max b1.bottom b2.bottom
+  , right:  Math.max b1.right  b2.right
+  }
+
+intersect :: Bounds -> Bounds -> Bounds
+intersect (Bounds b1) (Bounds b2) = Bounds
+  { top:    Math.max b1.top    b2.top
+  , left:   Math.max b1.left   b2.left
+  , bottom: Math.min b1.bottom b2.bottom
+  , right:  Math.min b1.right  b2.right
+  }
+
+emptyBounds :: Bounds
+emptyBounds = Bounds
+  { top:     Global.infinity
+  , left:    Global.infinity
+  , bottom: -Global.infinity
+  , right:  -Global.infinity
+  }
+
+infiniteBounds :: Bounds
+infiniteBounds = Bounds
+  { top:    -Global.infinity
+  , left:   -Global.infinity
+  , bottom:  Global.infinity
+  , right:   Global.infinity
+  }
+
+bounds :: Picture -> Bounds
+bounds = foldl combine emptyBounds
+  where
+  combine :: Bounds -> Shape -> Bounds
+  combine b shape = union (shapeBounds shape) b
+
+area :: Shape -> Number
+area (Circle _ r) = r * r * Math.pi
+area (Rectangle _ w h) = w * h
+area _ = 0.0
