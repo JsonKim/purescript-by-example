efa3b6e75a1799e14cbcfd78b3f034e01c4e9a23
Jason Kim <actcrazy@gmail.com>
2019-02-05 16:07:02 +0900

    add 5.14 exercises

--- chapter05/src/Data/Picture.purs
+++ chapter05/src/Data/Picture.purs
@@ -2,6 +2,8 @@ module Data.Picture where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
+
 data Point = Point
   { x :: Number
   , y :: Number
@@ -35,3 +37,22 @@ exampleLine = Line p1 p2
 
     p2 :: Point
     p2 = Point { x: 100.0, y: 50.0 }
+
+origin :: Point
+origin = Point { x, y }
+  where
+    x = 0.0
+    y = 0.0
+
+originCircle :: Shape
+originCircle = Circle origin 10.0
+
+scale :: Array Shape -> Array Shape
+scale = map scale' where
+  scale' (Circle _ r) = Circle origin (r * 2.0)
+  scale' (Rectangle _ w h) = Rectangle origin (w * 2.0) (h * 2.0)
+  scale' s = s
+
+getText :: Shape -> Maybe String
+getText (Text _ text) = Just text
+getText _ = Nothing
