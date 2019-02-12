83590cb6260544f73d38099692e61d7286e23881
Jason Kim <actcrazy@gmail.com>
2019-02-12 20:34:58 +0900

    add chapter 7 validation

--- chapter07/src/Data/AddressBook/Validation.purs
+++ chapter07/src/Data/AddressBook/Validation.purs
@@ -2,8 +2,13 @@ module Data.AddressBook.Validation where
   
 import Prelude
 
+import Data.AddressBook (Address(..), address)
+import Data.Either (Either(..))
 import Data.String (length)
+import Data.String.Regex (Regex, test, regex)
+import Data.String.Regex.Flags (noFlags)
 import Data.Validation.Semigroup (V, invalid)
+import Partial.Unsafe (unsafePartial)
 
 type Errors = Array String
 
@@ -16,3 +21,21 @@ lengthIs field len value | length value /= len =
   invalid ["Field '" <> field <> "' must have length " <> show len]
 lengthIs _     _   _     =
   pure unit
+
+validateAddress :: Address -> V Errors Address
+validateAddress (Address o) =
+  address <$> (nonEmpty "Street" o.street *> pure o.street)
+          <*> (nonEmpty "City"   o.city   *> pure o.city)
+          <*> (lengthIs "State" 2 o.state *> pure o.state)
+
+matches :: String -> Regex -> String -> V Errors Unit
+matches _ regex value | test regex value =
+  pure unit
+matches field _ _ =
+  invalid ["Field '" <> field <> "' did not match the required format"]
+
+phoneNumberRegex :: Regex
+phoneNumberRegex =
+  unsafePartial
+    case regex "^\\d{3}-\\d{3}-\\d{4}$" noFlags of
+      Right r -> r
