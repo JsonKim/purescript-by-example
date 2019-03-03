74055c5babe776a33f1fcbf77e46daec8df57605
Jason Kim <actcrazy@gmail.com>
2019-03-03 14:01:37 +0900

    add 8.7

---
+++ chapter08/src/Exercises.purs
@@ -0,0 +1,25 @@
+module Exercises where
+
+import Prelude
+
+import Data.Array (head, tail)
+import Data.List (List(..), (:), fromFoldable)
+import Data.Maybe (Maybe)
+
+foldM :: forall m a b. Monad m => (a -> b -> m a) -> a -> List b -> m a
+foldM _ a Nil = pure a
+foldM f a (b : bs) = do
+  a' <- f a b
+  foldM f a' bs
+
+third :: forall a. Array a -> Maybe a
+third arr = do
+  r1 <- tail arr
+  r2 <- tail r1
+  head r2
+
+add :: Int -> Int -> Array Int
+add x y = [x, y, x+y]
+
+sums :: Array Int -> Array Int
+sums xs = foldM add 0 (fromFoldable xs)
