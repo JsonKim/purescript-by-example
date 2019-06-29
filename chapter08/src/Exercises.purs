bc6cb22865fd6650555583013569be1a56010f94
USER <user@AD01152550.local>
2019-06-29 19:43:23 +0900

    klieisli compositon 설명 보충

--- chapter08/src/Exercises.purs
+++ chapter08/src/Exercises.purs
@@ -2,7 +2,9 @@ module Exercises where
 
 import Prelude
 
-import Data.Array (head, nub, sort, tail)
+import Control.Monad.ST (for, run)
+import Control.Monad.ST.Ref as Ref
+import Data.Array (head, length, nub, sort, tail)
 import Data.List (List(..), (:), fromFoldable)
 import Data.Maybe (Maybe)
 
@@ -64,12 +66,17 @@ filterM p (x:xs) = do
 -- (pure a >>= f) >>= g    = pure a >>= (\x -> f x >>= g) -> associativity
 -- left identity와 치환에 의해서
 -- (f a >>= g) = (f a >>= g)
+
+-- (>>=)의 타입을 생각해보자
+-- (>>=) :: forall m a b. Monad m => m a -> (a -> m b) -> m b
 -- 연산자를 flip하면
+-- (=<<) :: forall m a b. Monad m => (a -> m b) -> m a -> m b
 -- g =<< (f =<< pure a)
 -- g =<< f a
--- 이는 곧 모나드 함수들간의 합성을 의미한다.
 
--- 여기서 결국 kleisli compostion이 유도된다.
+-- 각 a, f, g 에 대한 타입 a :: a, f :: a -> m b, g :: b -> m c 에 대해서 함수를 작성한다면
+-- (<=<) :: forall m a b c. Monad m => (b -> m c) -> (a -> m b) -> a -> m c
+-- 가 되고, 이것이 kleisli composition이다.
 -- (>=>) :: forall a b c m. Monad m => (a -> m b) -> (b -> m c) -> a -> m c
 -- (>=>) f g a = f a >>= g
 -- (>=>) f g a = do
@@ -109,3 +116,28 @@ filterM p (x:xs) = do
 -- ap (map f (pure a)) (pure b)
 -- ap (pure (f a)) (pure b)
 -- pure ((f a) b)
+
+sumOfSquares :: Int
+sumOfSquares = run do
+  total <- Ref.new 0
+  Ref.read total
+
+simulate :: Number -> Number -> Int -> Number
+simulate x0 v0 time = run do
+  ref <- Ref.new { x: x0, v: v0 }
+  for 0 (time * 1000) \_ -> do
+    Ref.modify (\o ->
+      { v: o.v - 9.81 * 0.001
+      , x: o.x + o.v * 0.001
+      }
+    ) ref
+
+  final <- Ref.read ref
+  pure final.x
+
+check1 f l1 l2 = (f l1) == (f l2)
+x1 = check1 length [1,2,3] [2,3,4]
+
+check2 :: ∀ e. Eq e ⇒ (forall c. Array c → e) → (forall a. Array a) → (forall b. Array b) → Boolean
+check2 f l1 l2 = (f l1) == (f l2)
+x2 = check2 length [1,2,3] ["1"]
