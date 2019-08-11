28e577163ecc954993263ecd916bab97182e2f2d
Jason Kim <actcrazy@gmail.com>
2019-08-11 13:06:59 +0900

    feat: 기본 prompt 추가

--- chapter11/spago.dhall
+++ chapter11/spago.dhall
@@ -5,7 +5,7 @@ You can edit this file as you like.
 { name =
     "my-project"
 , dependencies =
-    [ "console", "effect", "psci-support", "yargs" ]
+    [ "console", "effect", "node-readline", "psci-support", "yargs" ]
 , packages =
     ./packages.dhall
 , sources =
--- chapter11/src/Main.purs
+++ chapter11/src/Main.purs
@@ -7,12 +7,26 @@ import Data.GameEnvironment (GameEnvironment, gameEnvironment)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
+import Node.ReadLine (close, createConsoleInterface, noCompletion, prompt, setLineHandler, setPrompt)
 import Node.Yargs.Applicative (Y, flag, runY, yarg)
 import Node.Yargs.Setup (usage)
 
 runGame :: GameEnvironment -> Effect Unit
 runGame env = do
-  log "it's game"
+  interface <- createConsoleInterface noCompletion
+  setPrompt "> " 2 interface
+
+  let
+    lineHandler :: String -> Effect Unit
+    lineHandler input =
+      if input == "quit"
+        then close interface
+        else do
+          log $ "You typed: " <> input
+          prompt interface
+
+  setLineHandler interface lineHandler
+  prompt interface
 
 main :: Effect Unit
 main = runY (usage "$0 -p <player name>") $ map runGame env
