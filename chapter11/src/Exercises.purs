47b946c9e959b7dc5ec990cf78f3780046a952ed
Jason Kim <actcrazy@gmail.com>
2019-08-11 17:09:42 +0900

    exercise:  11.5 Reader Monad

--- chapter11/src/Exercises.purs
+++ chapter11/src/Exercises.purs
@@ -2,10 +2,14 @@ module Exercises where
 
 import Prelude
 
+import Control.Monad.Reader (Reader, ask, local, runReader)
 import Control.Monad.State (State, evalState, execState, modify, runState)
+import Data.Array (replicate)
 import Data.Foldable (traverse_)
 import Data.Maybe (Maybe(..))
+import Data.String (joinWith)
 import Data.String.CodeUnits (toCharArray)
+import Data.Traversable (sequence)
 import Data.Tuple (Tuple)
 
 modifySum :: Int -> State Int Int
@@ -53,3 +57,37 @@ testParens input = case result of
 
     result :: Maybe Int
     result = execState (traverse_ calc (toCharArray input)) (Just 0)
+
+type Level = Int
+type DocA a = Reader Level a
+type Doc = DocA String
+
+line :: String -> Doc
+line string = do
+  level <- ask
+
+  pure $ (indentation level) <> string
+    where
+      indentation :: Int -> String
+      indentation x = joinWith "" (replicate x "  ")
+
+indent :: Doc -> Doc
+indent = local ((+) 1)
+
+cat :: Array Doc -> Doc
+cat x = joinWith "\n" <$> sequence x
+
+render :: Doc -> String
+render doc = runReader doc 0
+
+testDoc :: Array (DocA String)
+testDoc = [ line "Here is some indented text:"
+          , indent $ cat
+            [ line "I am indented"
+            , line "So am I"
+            , indent $ line "I am even more indented"
+            ]
+          ]
+
+z :: DocA (Array String)
+z = sequence testDoc
