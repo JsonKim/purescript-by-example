3f27b6ff0702d47ff32f7ab4a6f241475f664e7e
Jason Kim <actcrazy@gmail.com>
2019-09-14 22:27:06 +0900

    12.7 연습문제

--- chapter12/src/Exercise.purs
+++ chapter12/src/Exercise.purs
@@ -2,10 +2,16 @@ module Exercise where
 
 import Prelude
 
+import Control.Alt (alt, (<|>))
 import Control.Monad.Cont (ContT(..), runContT)
+import Control.Parallel (parallel, sequential)
 import Data.Function.Uncurried (Fn2, runFn2)
+import Data.Maybe (Maybe(..))
+import Data.Newtype (unwrap)
 import Effect (Effect)
 import Effect.Class.Console (log, logShow)
+import Files (readFileCont)
+import Network.HTTP.Client (URI, get)
 import Types (Async)
 
 type Milliseconds = Int
@@ -22,6 +28,20 @@ setTimeoutCont :: Milliseconds -> Async Unit
 setTimeoutCont ms = ContT $ setTimeout ms
 
 example :: Effect Unit
-example = runContT (do
+example = flip runContT pure do
   setTimeoutCont 1000
-  log "finish") logShow
+  log "finish1"
+  setTimeoutCont 1000
+  log "finish2"
+
+combineRequest :: URI -> URI -> Effect Unit
+combineRequest uri1 uri2 = flip runContT logShow do
+  sequential $
+    append
+      <$> parallel (get uri1)
+      <*> parallel (get uri2)
+
+timeout :: forall a. Milliseconds -> Async a -> Async (Maybe a)
+timeout ms fn = unwrap $
+    parallel (setTimeoutCont ms >>= \_ -> pure Nothing) <|>
+    parallel (fn >>= pure <<< Just)
