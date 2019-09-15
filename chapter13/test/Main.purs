baf3c8bc1febf6421d83b136bcf77488e1e22206
Jason Kim <actcrazy@gmail.com>
2019-09-15 12:19:09 +0900

    챕터 13.1 ~ 13.5

---
+++ chapter13/test/Main.purs
@@ -0,0 +1,50 @@
+module Test.Main where
+
+import Prelude
+
+import Data.Array (intersect, sort)
+import Data.List (List(..), fromFoldable)
+import Effect (Effect)
+import Merge (merge, mergePoly)
+import Test.QuickCheck ((<?>), quickCheck)
+
+isSorted :: forall a. (Ord a) => Array a -> Boolean
+isSorted = go <<< fromFoldable
+  where
+    go (Cons x1 t@(Cons x2 _)) = x1 <= x2 && go t
+    go _ = true
+
+isSubarrayOf :: forall a. (Eq a) => Array a -> Array a -> Boolean
+isSubarrayOf xs ys = xs `intersect` ys == xs
+
+ints :: Array Int -> Array Int
+ints = identity
+
+bools :: Array Boolean -> Array Boolean
+bools = identity
+
+main :: Effect Unit
+main = do
+  quickCheck \xs ys -> (isSorted $ merge (sort xs) (sort ys))
+    <?> "머지 결과가 정렬되지 않았습니다. " <> show (sort xs) <> ", " <> show (sort ys)
+
+  quickCheck \xs ys -> xs `isSubarrayOf` merge xs ys
+    <?> "xs가 머지 결과의 부분 배열이 아닙니다. " <> show xs <> ", " <> show ys
+
+  quickCheck \xs ys ->
+    let 
+      result = merge (sort xs) (sort ys)
+    in
+      xs `isSubarrayOf` result <?> show xs <> " not a subarray of" <> show result
+
+  quickCheck \xs -> (merge xs []) == xs
+    <?> "Merging " <> show xs <> " with [] doesn't produce " <> show xs <> "."
+
+  quickCheck \xs -> (merge [] xs) == xs
+    <?> "Merging " <> show xs <> " with [] doesn't produce " <> show xs <> "."
+
+  quickCheck \xs ys -> isSorted $ ints $ mergePoly (sort xs) (sort ys)
+  quickCheck \xs ys -> ints xs `isSubarrayOf` mergePoly xs ys
+
+  quickCheck \xs ys -> isSorted $ bools $ mergePoly (sort xs) (sort ys)
+  quickCheck \xs ys -> bools xs `isSubarrayOf` mergePoly xs ys
